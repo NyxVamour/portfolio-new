a1a7e0da128b8182a46033eb1fc39a9b13a8c432
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-02-02 15:40:54 -0500

    style: complete styling MainBGElements

--- app/features/masterLayout/components/MainBGElements.tsx
+++ app/features/masterLayout/components/MainBGElements.tsx
@@ -1,36 +1,58 @@
+import BGElementHorizontal from "./BGElementHorizontal";
+import BGElementVertical from "./BGElementVertical";
 import styles from "./mainBGElements.module.css";
 
 export default function MainBGElements() {
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.topData}>
-                <p>SECURITY CAM FEED</p>
-                <p>REC</p>
-                <p>125 // 2500 f.18</p>
-                <p>12:58 PM</p>
+        <div className={` ${styles.mainContainer}`}>
+            <div className={`opacity-40 ${styles.topData}`}>
+                <div className={`glowWhite ${styles.boxContainer}`}>
+                    <p>SECURITY CAM FEED</p>
+                </div>
+                <p className="text-red-500 glowRed">REC ●</p>
+                <p className="glowWhite">125 // 2500 f.18</p>
+                <p className="glowWhite">12:58 PM</p>
             </div>
-            <div className={styles.bottomData}>
-                <p>TEMP: -10°</p>
-                <p>WIND: 15km/h</p>
-                <p>SIGNAL: HIGH</p>
-                <p>43.642987° N, -79.391809° W</p>
+            <div className={`opacity-40 ${styles.bottomData}`}>
+                <p className="glowWhite">TEMP: -10°</p>
+                <p className="glowWhite">WIND: 15km/h</p>
+                <p className="glowWhite">SIGNAL: HIGH</p>
+                <p className="glowWhite">43.642987° N, -79.391809° W</p>
             </div>
-            <div className={styles.targetContainer}>
-                <p>Target Locked</p>
+            <div className={`flex-col ${styles.targetContainer}`}>
+                <p className="glowWhite text-[8px] opacity-40">Target Locked</p>
                 <img
                     src="/images/target.png"
                     alt="Photo of Charlize in an all black outfit and sunglasses drinking a cup of coffee."
+                    className="border glowWhite"
                 ></img>
-                <div>
-                    <p>BPM</p>
-                    <p>78</p>
-                </div>
-                <div>
-                    <p>SYS</p>
-                    <p>120</p>
+                <div className="flex gap-2 justify-end mt-1 opacity-40">
+                    <div className="flex flex-col ">
+                        <div
+                            className={`glowWhite ${styles.boxContainer} text-[8px]`}
+                        >
+                            <p className="glowWhite">BPM</p>
+                        </div>
+                        <p className="self-end">78</p>
+                    </div>
+                    <div className="flex flex-col">
+                        <div
+                            className={`glowWhite ${styles.boxContainer} text-[8px]`}
+                        >
+                            <p className="glowWhite">SYS</p>
+                        </div>
+                        <p className="self-end">120</p>
+                    </div>
                 </div>
             </div>
-            <div></div>
+            <div className={styles.linesContainer}>
+                <BGElementVertical
+                    className={styles.bgElement__vertical}
+                ></BGElementVertical>
+                <BGElementHorizontal
+                    className={styles.bgElement__horizontal}
+                ></BGElementHorizontal>
+            </div>
         </div>
     );
 }
