6ef07223b30395d5ca9a1850decdfa54f1c1b07b
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-02-02 14:19:57 -0500

    feat: create masterLayout.tsx and add bg

---
+++ app/features/masterLayout/MasterLayout.tsx
@@ -0,0 +1,9 @@
+import styles from "./masterLayout.module.css";
+
+export default function MasterLayout() {
+    return (
+        <main className={styles.masterLayout}>
+            <div className={styles.vignette}></div>
+        </main>
+    );
+}
