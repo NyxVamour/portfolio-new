a9d0684cd87c370851ab3cc2b55d6b4bbaf68b53
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-02-02 16:27:55 -0500

    style: add strength to css glows

--- app/features/masterLayout/components/MainBGElements.tsx
+++ app/features/masterLayout/components/MainBGElements.tsx
@@ -6,42 +6,44 @@ export default function MainBGElements() {
     return (
         <div className={` ${styles.mainContainer}`}>
             <div className={`opacity-40 ${styles.topData}`}>
-                <div className={`glowWhite ${styles.boxContainer}`}>
+                <div className={`glowWhite--strong ${styles.boxContainer}`}>
                     <p>SECURITY CAM FEED</p>
                 </div>
-                <p className="text-red-500 glowRed">REC ●</p>
-                <p className="glowWhite">125 // 2500 f.18</p>
-                <p className="glowWhite">12:58 PM</p>
+                <p className="text-red-500 glowRed--strong">REC ●</p>
+                <p className="glowWhite--strong">125 // 2500 f.18</p>
+                <p className="glowWhite--strong">12:58 PM</p>
             </div>
             <div className={`opacity-40 ${styles.bottomData}`}>
-                <p className="glowWhite">TEMP: -10°</p>
-                <p className="glowWhite">WIND: 15km/h</p>
-                <p className="glowWhite">SIGNAL: HIGH</p>
-                <p className="glowWhite">43.642987° N, -79.391809° W</p>
+                <p className="glowWhite--strong">TEMP: -10°</p>
+                <p className="glowWhite--strong">WIND: 15km/h</p>
+                <p className="glowWhite--strong">SIGNAL: HIGH</p>
+                <p className="glowWhite--strong">43.642987° N, -79.391809° W</p>
             </div>
             <div className={`flex-col ${styles.targetContainer}`}>
-                <p className={`glowWhite opacity-40 ${styles.targetText}`}>
+                <p
+                    className={`glowWhite--strong opacity-40 ${styles.targetText}`}
+                >
                     Target Locked
                 </p>
                 <img
                     src="/images/target.png"
                     alt="Photo of Charlize in an all black outfit and sunglasses drinking a cup of coffee."
-                    className={`border glowWhite ${styles.targetImage}`}
+                    className={`border glowWhite--medium ${styles.targetImage}`}
                 ></img>
                 <div className="flex gap-2 justify-end mt-1 opacity-40">
                     <div className="flex flex-col ">
                         <div
-                            className={`glowWhite ${styles.boxContainer} text-[8px]`}
+                            className={`glowWhite--strong ${styles.boxContainer} text-[8px]`}
                         >
-                            <p className="glowWhite">BPM</p>
+                            <p className="glowWhite--strong">BPM</p>
                         </div>
                         <p className="self-end">78</p>
                     </div>
                     <div className="flex flex-col">
                         <div
-                            className={`glowWhite ${styles.boxContainer} text-[8px]`}
+                            className={`glowWhite--strong ${styles.boxContainer} text-[8px]`}
                         >
-                            <p className="glowWhite">SYS</p>
+                            <p className="glowWhite--strong">SYS</p>
                         </div>
                         <p className="self-end">120</p>
                     </div>
