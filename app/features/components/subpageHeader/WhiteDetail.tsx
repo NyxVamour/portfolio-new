c5a6e323ff1888d2e6b1cd977b6abdb3fa93c9e9
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-03-26 14:06:20 -0400

    feat: create white detail component

---
+++ app/features/components/subpageHeader/WhiteDetail.tsx
@@ -0,0 +1,15 @@
+export default function WhiteDetail() {
+    return (
+        <svg
+            width="100%"
+            height="2"
+            viewBox="0 0 1040 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="whiteDetail"
+            preserveAspectRatio="none"
+        >
+            <rect width="1040" height="2" fill="white" />
+        </svg>
+    );
+}
