35019a3e6ad9e9b1712a9ff5d2014e4ecae90cff
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-04-08 16:00:31 -0400

    feat: create a projectCard component

---
+++ app/features/projects/components/ProjectCard.tsx
@@ -0,0 +1,52 @@
+import styles from "./projectCard.module.css";
+
+type ProjectCardProps = {
+    imgSrc: string;
+    title: string;
+    description: string;
+    stack: string[];
+};
+
+export default function ProjectCard({
+    imgSrc,
+    title,
+    description,
+    stack,
+}: ProjectCardProps) {
+    return (
+        <article className={`${styles.projectCard}`}>
+            <figure className={`${styles.imgWrapper}`}>
+                <img
+                    src={`/images/${imgSrc}`}
+                    alt={``}
+                    className={`${styles.projectImg}`}
+                />
+            </figure>
+            <div className={`${styles.cardDetailsWrapper}`}>
+                <div className={`${styles.cardTopDetailsWrapper}`}>
+                    <div className={`${styles.titleWrapper}`}>
+                        <h2 className={`${styles.title}`}>{title}</h2>
+                    </div>
+                    <p>{description}</p>
+                </div>
+                <div className={`${styles.cardBottomDetailsWrapper}`}>
+                    <ul className={`${styles.stackList}`}>
+                        {stack.map((tech) => (
+                            <li key={tech} className={`${styles.stackItem}`}>
+                                {tech}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={`${styles.buttonWrapper}`}>
+                        <button className={`${styles.button}`}>
+                            Read More
+                        </button>
+                        <button className={`${styles.button}`}>
+                            Visit Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
+}
