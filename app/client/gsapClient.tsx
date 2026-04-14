e9a028d7bbd8daff2ebce2cc788d8a96f8fbcc9e
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-04-14 18:30:24 -0400

    feat: add scrambletext and textplugin to gsap

--- app/client/gsapClient.tsx
+++ app/client/gsapClient.tsx
@@ -1,3 +1,7 @@
 "use client";
 import gsap from "gsap";
+import ScrambleTextPlugin from "gsap/ScrambleTextPlugin";
+import TextPlugin from "gsap/TextPlugin";
+
+gsap.registerPlugin(ScrambleTextPlugin, TextPlugin);
 export default gsap;
