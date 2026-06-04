60546227d08a011654755919179d4ed9f9ce61b1
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-06-04 14:34:18 -0400

    fix: use client side rendering for clock

--- app/features/masterLayout/functions/Clock.tsx
+++ app/features/masterLayout/functions/Clock.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 export default function Clock() {
-    const [time, setTime] = useState<Date>(new Date());
+    const [time, setTime] = useState("");
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
+        const updateTime = () => setTime(new Date().toLocaleTimeString());
+        updateTime();
+
+        const interval = setInterval(updateTime, 1000);
 
         return () => clearInterval(interval);
     }, []);
 
-    return time.toLocaleTimeString();
+    return time;
 }
