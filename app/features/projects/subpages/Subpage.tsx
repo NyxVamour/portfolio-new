4a4c09d38b83ccf093a2af44a89aca855be6a717
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-06-10 15:18:59 -0400

    feat: create Summary component and move html to it

--- app/features/projects/subpages/Subpage.tsx
+++ app/features/projects/subpages/Subpage.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 import styles from "./subpage.module.css";
 import IconBackArrow from "~/features/components/icons/IconBackArrow";
+import Summary from "./components/Summary";
+import EdBroadbent from "./projects/EdBroadbent";
 
 type SubpageProps = {
     subpageRef?: React.Ref<HTMLDivElement>;
@@ -31,74 +33,8 @@ export default function Subpage({
                     </div>
                 </header>
                 <main>
-                    <section className={`${styles.summary}`}>
-                        <figure className={`${styles.summary__figure}`}>
-                            <img
-                                className={`${styles.summary__img}`}
-                                src="/images/portfolio01.png"
-                            ></img>
-                        </figure>
-                        <div className={`${styles.informationFrame}`}>
-                            <div className={`${styles.mainInfo}`}>
-                                <p className={`${styles.mainInfo__category}`}>
-                                    Web App
-                                </p>
-                                <h1 className={`${styles.mainInfo__title}`}>
-                                    Ed Broadbent Waterfront Park Pocket Guide
-                                </h1>
-                                <Link
-                                    className={`${styles.mainInfo__btn}`}
-                                    to={"#"}
-                                >
-                                    Visit Site
-                                </Link>
-                            </div>
-                            <div className={`${styles.addInfo}`}>
-                                <div className={`${styles.addInfoLeft}`}>
-                                    <div
-                                        className={`${styles.addInfo__wrapper}`}
-                                    >
-                                        <p
-                                            className={`${styles.addInfo__title}`}
-                                        >
-                                            Year
-                                        </p>
-                                        <p
-                                            className={`${styles.addInfo__descript}`}
-                                        >
-                                            2025
-                                        </p>
-                                    </div>
-                                    <div
-                                        className={`${styles.addInfo__wrapper}`}
-                                    >
-                                        <p
-                                            className={`${styles.addInfo__title}`}
-                                        >
-                                            Type
-                                        </p>
-                                        <p
-                                            className={`${styles.addInfo__descript}`}
-                                        >
-                                            Co-op
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className={`${styles.addInfo__wrapper}`}>
-                                    <p className={`${styles.addInfo__title}`}>
-                                        Tech Stack
-                                    </p>
-                                    <ul>
-                                        <li>HTML</li>
-                                        <li>CSS</li>
-                                        <li>JavaScript</li>
-                                        <li>React</li>
-                                        <li>TypeScript</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    <Summary />
+                    <EdBroadbent />
                 </main>
             </div>
         </div>
