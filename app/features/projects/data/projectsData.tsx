2180d68176f404041ac9a574c1abe6b791d1041d
Charlize San Gabriel <charlizesangabriel@gmail.com>
2026-06-10 17:03:27 -0400

    chore: remove url from projectsData

--- app/features/projects/data/projectsData.tsx
+++ app/features/projects/data/projectsData.tsx
@@ -13,7 +13,6 @@ export const projectsData = [
     {
         id: 1,
         title: "Ed Broadbent Waterfront Park Pocket Guide",
-        url: "ed_broadbent_park_pocket_guide",
         description:
             "Working with TeachingCity Oshawa, we made an interactive map of the Ed Broadbent Waterfront Park in Oshawa. Features different points of interest and information about them.",
         imgSrc: "portfolio01.png",
@@ -33,7 +32,6 @@ export const projectsData = [
     {
         id: 2,
         title: "Urban Sprouts Academy",
-        url: "urban_sprouts_academy",
         description:
             "A gaming hub with two educational children's games teaching about plants.",
         imgSrc: "portfolio02.png",
@@ -44,7 +42,6 @@ export const projectsData = [
     {
         id: 3,
         title: "Goober the Owl Monster",
-        url: "goober_the_owl_monster",
         description:
             "Tamagotchi project featuring Goober, the cutest, sassiest owl monster. Voiced by me. Music and SFX from Pixabay.",
         imgSrc: "portfolio06.png",
@@ -55,7 +52,6 @@ export const projectsData = [
     {
         id: 5,
         title: "Happy Hunting",
-        url: "happy_hunting",
         description:
             "This is the first game I made using vanilla JavaScript. It features omnidirectional movement, a dash, and directional attacks. The game also spawns two types of enemies, a passive, and hostile mob. Art made by me. I also made music using Tone.js, and got SFX from Pixabay.",
         imgSrc: "portfolio07.png",
@@ -66,7 +62,6 @@ export const projectsData = [
     {
         id: 6,
         title: "Grocer-ease",
-        url: "grocer-ease",
         description:
             "This is my grocery website project. We were tasked with making a grocery website. It pulls the info from a database and displays it. This also features searching and an add to cart functionality.",
         imgSrc: "portfolio04.png",
@@ -85,7 +80,6 @@ export const projectsData = [
     {
         id: 7,
         title: "Board Hoard",
-        url: "board_hoard",
         description:
             "This is my first WordPress site built using my own custom theme. I made a nerdy website that sells board games, comics, miniatures, and other nerdy stuff galore! It also features a blog section, where you can post events, stories, and reviews.",
         imgSrc: "portfolio08.png",
@@ -96,7 +90,6 @@ export const projectsData = [
     {
         id: 8,
         title: "Yappee!",
-        url: "yappee",
         description:
             "A social and drinking party game featuring different categories to talk about, act out, and more!",
         imgSrc: "portfolio05.png",
@@ -107,7 +100,6 @@ export const projectsData = [
     {
         id: 9,
         title: "Community Database",
-        url: "community_db",
         description:
             "A database of the hit comedy TV show “Community”. Contains information of all the episodes and cast.",
         imgSrc: "portfolio09.png",
